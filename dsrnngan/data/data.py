fixed and tested problem with input normalisation

dsrnngan/data/data.py
@@ -408,7 +408,7 @@ def load_hdf5_file(fp: str, group_name:str ='Grid'):
 
 def preprocess(variable: str, 
                ds: xr.Dataset, 
-               var_name_lookup: dict, 
+               normalisation_strategy: dict, 
                stats_dict: dict=None):
     """
     Preprocess data
@@ -416,7 +416,7 @@ def preprocess(variable: str,
     Args:
         variable (str): name of variable.
         ds (xr.Dataset): dataset containing data for named variable
-        var_name_lookup (dict): dict containing normalisation strategy for each variable
+        normalisation_strategy (dict): dict containing normalisation strategy for each variable
         stats_dict (dict, optional): dict of values required for normalisation. Defaults to None.
 
     Returns:
@@ -426,7 +426,7 @@ def preprocess(variable: str,
     
     var_name = list(ds.data_vars)[0]
 
-    normalisation_type = var_name_lookup[variable].get('normalisation')
+    normalisation_type = normalisation_strategy[variable].get('normalisation')
 
     if normalisation_type:
 
@@ -596,6 +596,7 @@ def load_fcst_radar_batch(batch_dates: Iterable,
                           obs_data_source: str, 
                           fcst_dir: str,
                           obs_data_dir: str,
+                          normalisation_strategy: dict,
                           latitude_range: Iterable[float]=None,
                           longitude_range: Iterable[float]=None,
                           constants_dir: str=CONSTANTS_PATH,
@@ -621,7 +622,8 @@ def load_fcst_radar_batch(batch_dates: Iterable,
         h = hours[i]
         batch_x.append(load_fcst_stack(fcst_data_source, fcst_fields, date, h,
                                        latitude_vals=latitude_range, longitude_vals=longitude_range, fcst_dir=fcst_dir,
-                                       norm=normalise_inputs, constants_dir=constants_dir))
+                                       norm=normalise_inputs, constants_dir=constants_dir,
+                                       normalisation_strategy=normalisation_strategy))
         
         if obs_data_source is not None:
             batch_y.append(load_observational_data(obs_data_source, date, h, normalisation_type=output_normalisation,
@@ -780,10 +782,10 @@ def load_ifs_raw(field: str,
     return ds
 
 def load_ifs(field: str, 
-             date, hour: int, 
+             date, hour: int,
+             normalisation_strategy: dict,
              norm: bool=False, 
-             fcst_dir: str=IFS_PATH, 
-             var_name_lookup: dict=VAR_LOOKUP_IFS,
+             fcst_dir: str=IFS_PATH,
              latitude_vals: list=None, 
              longitude_vals: list=None, 
              constants_path: str=CONSTANTS_PATH):
@@ -796,7 +798,7 @@ def load_ifs(field: str,
         hour (int): hour to forecast
         norm (bool, optional): whether or not to normalise the data. Defaults to False.
         fcst_dir (str, optional): forecast data directory. Defaults to IFS_PATH.
-        var_name_lookup (str, optional): dict with normalisation details for variables. Defaults to VAR_LOOKUP_IFS.
+        normalisation_strategy (str, optional): dict with normalisation details for variables. Defaults to VAR_LOOKUP_IFS.
         latitude_vals (list, optional): latitude values to filter/interpolate to. Defaults to None.
         longitude_vals (list, optional): longitude_vals to filter/interpolate to. Defaults to None.
         constants_path (str, optional): path to constant data. Defaults to CONSTANTS_PATH.
@@ -813,7 +815,7 @@ def load_ifs(field: str,
     
     var_name = list(ds.data_vars)[0]
     
-    if not var_name_lookup[field].get('negative_vals', True):
+    if not normalisation_strategy[field].get('negative_vals', True):
         # Make sure no negative values
         ds[var_name] = ds[var_name].clip(min=0)
         
@@ -830,7 +832,7 @@ def load_ifs(field: str,
                             use_cached=True, ifs_data_dir=fcst_dir,
                             output_dir=constants_path)
         # Normalisation here      
-        ds = preprocess(field, ds, stats_dict=stats_dict, var_name_lookup=var_name_lookup)
+        ds = preprocess(field, ds, stats_dict=stats_dict, normalisation_strategy=normalisation_strategy)
     
     y = np.array(ds[var_name][:, :])
     
@@ -840,6 +842,7 @@ def load_ifs(field: str,
 def load_fcst_stack(data_source: str, fields: list, 
                     date: str, hour: int, 
                     fcst_dir: str, 
+                    normalisation_strategy: dict,
                     constants_dir:str=CONSTANTS_PATH,
                     norm:bool=False,
                     latitude_vals:list=None, longitude_vals:list=None):
@@ -852,6 +855,7 @@ def load_fcst_stack(data_source: str, fields: list,
         date (str): YYYYMMDD date string to forecast for
         hour (int): hour to forecast for
         fcst_dir (str): folder with forecast data in
+        normalisation_strategy (dict): normalisation strategy
         constants_dir (str, optional): folder with constants data in. Defaults to CONSTANTS_PATH.
         norm (bool, optional): whether or not to normalise the data. Defaults to False.
         latitude_vals (list, optional): list of latitude values. Defaults to None.
@@ -872,7 +876,8 @@ def load_fcst_stack(data_source: str, fields: list,
     for f in fields:
         field_arrays.append(load_function(f, date, hour, fcst_dir=fcst_dir,
                                           latitude_vals=latitude_vals, longitude_vals=longitude_vals,
-                                          constants_path=constants_dir, norm=norm))
+                                          constants_path=constants_dir, norm=norm,
+                                          normalisation_strategy=normalisation_strategy))
     return np.stack(field_arrays, -1)
 
 

system_tests/test_data_generator.py
@@ -58,7 +58,7 @@ class TestDataGenerator(unittest.TestCase):
                             'max_longitude': 34.95, 
                             'longitude_step_size': 0.1, 
                             'normalisation_strategy': data.IFS_NORMALISATION_STRATEGY,
-                            'input_fields': ['2t', 'cape', 'cp', 'r200', 'r700', 'r950'], 
+                            'input_fields': ['tp', '2t', 'cape', 'cp', 'r200', 'r700', 'r950'], 
                             'constant_fields': ['lakes', 'sea', 'orography'], 
                             'paths': {'BLUE_PEBBLE':
                                 {'GENERAL': {'IMERG': str(data_folder/ 'IMERG/half_hourly/final'),
@@ -138,7 +138,48 @@ class TestDataGenerator(unittest.TestCase):
         data_sqrt_norm = [data_gen_sqrt_norm[n] for n in range(len(date_range))]
         np.testing.assert_allclose(data.denormalise(data_sqrt_norm[0][1]['output'], 'sqrt'), data_no_norm[0][1]['output'], atol=1e-5)
         np.testing.assert_allclose(np.power(data_sqrt_norm[0][1]['output'], 2), data_no_norm[0][1]['output'], atol=1e-5)
+  
+    def test_input_normalisation(self):
+
+        date_range = [datetime(2017,7,4), datetime(2017,7,4)]
+        batch_size = 2
+        tpidx = self.data_config.input_fields.index('tp')
         
+        no_norm_data_config = copy.deepcopy(self.data_config)
+        no_norm_data_config.normalise_inputs = False
+        data_gen_no_norm = DataGenerator(date_range, batch_size=batch_size,
+                            data_config=no_norm_data_config,
+                        shuffle=False, hour=17,
+                        downsample=False, seed=None,
+                        repeat_data=True)
+        
+        data_no_norm = [data_gen_no_norm[n] for n in range(len(date_range))]
+
+        log_norm_data_config = copy.deepcopy(self.data_config)
+        log_norm_data_config.normalise_inputs = True
+        log_norm_data_config.normalisation_strategy['tp']['normalisation'] = 'log'
+        data_gen_log_norm = DataGenerator(date_range, batch_size=batch_size,
+                            data_config=log_norm_data_config,
+                        shuffle=False, hour=17,
+                        downsample=False, seed=None,
+                        repeat_data=True)
+        
+        data_log_norm = [data_gen_log_norm[n] for n in range(len(date_range))]
+        np.testing.assert_allclose(data.denormalise(data_log_norm[0][0]['lo_res_inputs'][...,tpidx], 'log'), data_no_norm[0][0]['lo_res_inputs'][...,tpidx], atol=1e-6)
+
+        
+        sqrt_norm_data_config = copy.deepcopy(self.data_config)
+        sqrt_norm_data_config.normalise_inputs = True
+        sqrt_norm_data_config.normalisation_strategy['tp']['normalisation'] = 'sqrt'
+        data_gen_sqrt_norm = DataGenerator(date_range, batch_size=batch_size,
+                            data_config=sqrt_norm_data_config,
+                        shuffle=False, hour=17,
+                        downsample=False, seed=None,
+                        repeat_data=True)
+        data_sqrt_norm = [data_gen_sqrt_norm[n] for n in range(len(date_range))]
+
+        np.testing.assert_allclose(data.denormalise(data_sqrt_norm[0][0]['lo_res_inputs'][...,tpidx], 'sqrt'), data_no_norm[0][0]['lo_res_inputs'][...,tpidx], atol=1e-6)
+      
     def test_repeat_data(self):
         # Test that generator keeps producing if repeat_data is True
         date_range = [datetime(2017,7,4), datetime(2017,7,5)]