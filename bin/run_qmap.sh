f0619578746d7458a88bfa182ab4ef69863ae2f3
Bobby Antonio <bobby.antonio@physics.ox.ac.uk>
2024-06-12 15:24:40 +0100

    Adapating to aopp cluster

--- bin/run_qmap.sh
+++ bin/run_qmap.sh
@@ -4,17 +4,13 @@
 #SBATCH --mem=150gb
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=qmap
-#SBATCH --partition short,dmm,compute
+#SBATCH --partition shared
 #SBATCH --output=logs/qmap-%A_%a.out
-#SBATCH --array=1,15
 
 source ~/.bashrc
 echo Running on host `hostname`
 echo Time is `date`
 echo Directory is `pwd`
-echo Slurm job ID is $SLURM_JOBID
-echo This jobs runs on the following machines:
-echo $SLURM_JOB_NODELIST
 
 # print out stuff to tell you when the script is running
 echo running quantile mapping
@@ -23,7 +19,8 @@ echo "$dt"
 
 # either run the script to train your model
 # srun python -m scripts.quantile_mapping --num-lat-lon-chunks ${SLURM_ARRAY_TASK_ID} --log-folder /user/work/uz22147/logs/cgan/7c4126e641f81ae0_medium-cl100-final-nologs --model-number 217600 --output-folder plots/quantile_map_plots --plot;
-srun python -m scripts.quantile_mapping --num-lat-lon-chunks ${SLURM_ARRAY_TASK_ID}  --log-folder /user/work/uz22147/logs/cgan/7c4126e641f81ae0_medium-cl100-final-nologs --model-number 217600 --output-folder plots/quantile_map_plots --save-qmapper;
+srun python -m scripts.quantile_mapping --num-lat-lon-chunks 2 --model-eval-folder /network/group/aopp/predict/HMC005_ANTONIO_EERIE/cgan_data/7c4126e641f81ae0_medium-cl100-final-nologs/n8640_202010-202109_45682_e1 --model-number 217600 --save-data;
+srun python -m scripts.quantile_mapping --num-lat-lon-chunks 3 --model-eval-folder /network/group/aopp/predict/HMC005_ANTONIO_EERIE/cgan_data/7c4126e641f81ae0_medium-cl100-final-nologs/n8640_202010-202109_45682_e1 --model-number 217600 --save-data;
 
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo "$dt"
